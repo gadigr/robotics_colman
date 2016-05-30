0cfbd900441d6bc292a740aa650e8f513ae2a1f9
RotemShaanan <shaanan.rotem@gmail.com>
2016-05-30 06:13:01 -0700

    with the drive now

---
+++ Driver.cpp
@@ -0,0 +1,79 @@
+/*
+ * Driver.cpp
+ *
+ *  Created on: May 30, 2016
+ *      Author: colman
+ */
+
+#include "Driver.h"
+#include <cmath>
+
+Driver::Driver(Robot *robot) : robot(robot) {
+
+}
+
+double Driver::distance(double x1, double y1, double x2, double y2) {
+	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+
+
+void Driver::moveToNextWaypoint(double x, double y) {
+	robot->read();
+	double currX = robot->getXPosition();
+	double currY = robot->getYPosition();
+	double currYaw = robot->getYawPosition();
+	cout << "x: " << currX << ", y: " << currY << ", Yaw: " << currYaw * 180/PI<<  endl;
+	double dist = distance(currX, currY, x, y);
+
+	double angle4 = atan2(y - currY, x - currX);
+	double asd = angle4 *180/M_PI;
+
+
+	// TODO: change the robot angle until it is looking for the next waypoint
+	// This calculation in the while is wrong
+	while (abs((currYaw*180/M_PI) - angle4*180/M_PI) > angleTolerange) {
+
+
+		// here we need to calc how to rotate the robot - left or right
+		// and rotate the robot a little
+		robot->setSpeed(0, yawSpeed*((angle4)/abs(angle4)));
+
+		// read the robot angle again
+		robot->read();
+		currYaw = robot->getYawPosition();
+
+		cout << "yaw: " << (currYaw) *180/ PI << " fin: " << angle4 *180/PI << endl;
+	}
+
+	robot->setSpeed(0, 0);
+
+	while (distance(currX, currY, x, y) > slowSpeedRange) {
+		robot->setSpeed(linearSpeed, 0);
+
+		robot->read();
+		currX = robot->getXPosition();
+		currY = robot->getYPosition();
+		currYaw = robot->getYawPosition();
+			cout << ">fast   x: " << currX << ", y: " << currY << ", Yaw: " <<
+					currYaw*180/PI<<  endl;
+	}
+
+	while (distance(currX, currY, x, y) > tolerance) {
+		robot->setSpeed(linearSpeed * slowSpeedRatio, 0);
+
+		robot->read();
+		currX = robot->getXPosition();
+		currY = robot->getYPosition();
+		currYaw = robot->getYawPosition();
+		cout << ">slow   x: " << currX << ", y: " << currY << ", Yaw: " << currYaw *180/M_PI<<  endl;
+	}
+
+	robot->setSpeed(0, 0);
+}
+
+Driver::~Driver() {
+	// TODO Auto-generated destructor stub
+}
+
+
---
+++ Driver.h
@@ -0,0 +1,33 @@
+/*
+ * Driver.h
+ *
+ *  Created on: May 30, 2016
+ *      Author: colman
+ */
+
+#ifndef DRIVER_H_
+#define DRIVER_H_
+#include "robot.h"
+#include <math.h>
+
+#define PI 3.14159265
+
+class Driver {
+private:
+	const double angleTolerange = 1;
+	const double tolerance = 0.02;
+	const double slowSpeedRange = 10 * tolerance;
+	const double linearSpeed = 0.5;
+	const double yawSpeed = 0.2;
+	const double slowSpeedRatio = 0.1;
+
+	Robot *robot;
+	double distance(double x1, double y1, double x2, double y2);
+
+public:
+	Driver(Robot *robot);
+	void moveToNextWaypoint(double x, double y);
+	virtual ~Driver();
+};
+
+#endif /* DRIVER_H_ */
